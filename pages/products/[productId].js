3ac1ca352b790dc078f6f1df3e276f74428bb0e6
Bilal <mansuribilal101@gmail.com>
2021-09-07 13:19:28 +0530

    added staticprops to product detail page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,8 +1,9 @@
 import ProductDetail from "../../components/productDetails/product-detail";
-import { server } from '../../config/index';
+import { connectToDatabase } from '../../util/database';
 function ProductDetailsPage(props) {
+    const product = JSON.parse(props.product)
     return (
-        <ProductDetail product={props.product} />
+        <ProductDetail product={product} />
     )
 }
 export default ProductDetailsPage;
@@ -10,11 +11,39 @@ export default ProductDetailsPage;
 
 export async function getStaticProps(context) {
     const { productId } = context.params;
-    const res = await fetch(`${server}/api/product-details/${productId}`);
-    const data = await res.json();
-    return {
-        props: {
-            product: data.product
+    let client;
+    //connecting to database
+    try {
+        client = await connectToDatabase();
+    }
+    catch (error) {
+        client.close();
+        return {
+            props: {
+                error: 'Cannot connect to database',
+                product: null,
+            }
+        }
+    }
+    //find the product with id same as give in query 
+    try {
+        const product = await client.db().collection('products').findOne({ productId: productId });
+        console.log(product)
+        client.close();
+        return {
+            props: {
+                error: null,
+                product: JSON.stringify(product),
+            }
+        }
+    }
+    catch (err) {
+        client.close();
+        return {
+            props: {
+                error: 'Product not found',
+                product: null,
+            }
         }
     }
 }
