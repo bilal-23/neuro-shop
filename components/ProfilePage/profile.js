e9d6a2202d167a9b2f0cf103f61337fb59e6758e
Bilal <mansuribilal101@gmail.com>
2021-10-19 09:58:07 +0530

    FIX: signout bug

--- components/ProfilePage/profile.js
+++ components/ProfilePage/profile.js
@@ -3,14 +3,17 @@ import OrderItem from "./order-item";
 import { useDispatch } from "react-redux";
 import { cartActions } from '../../store/cart-slice';
 import classes from './profile.module.css';
+import { useSession } from 'next-auth/client';
 
 function Profile({ orders }) {
     const dispatch = useDispatch();
 
     async function logoutHandler() {
         dispatch(cartActions.resetCart());
-        signout({ redirect: false, callbackUrl: "/" });
-        location.replace('/');
+        const data = await signout({ redirect: false, callbackUrl: "/" });
+        if (data) {
+            location.replace('/');
+        }
     }
     return (
         <section className={classes.profile}>
